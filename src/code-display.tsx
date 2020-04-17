e59bf0b5b553629cbc6e530a3085c96ef34be7b7
HairyMaclary <cliff.hamilton@outlook.com>
2020-04-18 11:53:35 +1200

    more transforms

--- src/code-display.tsx
+++ src/code-display.tsx
@@ -3,11 +3,13 @@ import { ICode } from './container';
 import './code-display.css'
 
 export interface ICodeDisplayProps {
-    code: ICode, updateCode: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
+    code: ICode
+    updateCode: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
 }
 
 export interface ICodeDisplayState {
-    cssDisplay: string
+    cssDisplay: string,
+    displayHTML: boolean;
 }
 
 // export function CodeDisplay(props: {code: ICode, updateCode: any}) {
@@ -15,14 +17,16 @@ export class CodeDisplay extends React.Component<ICodeDisplayProps, ICodeDisplay
 
     constructor(props: ICodeDisplayProps) {
         super(props);
-        this.state = { cssDisplay: props.code.css }
+        this.state = { cssDisplay: props.code.css, displayHTML: false }
     }
 
     // This is a workaround to the value attribute in textarea not updating.
     // But, we need to be careful that we don't miss out on wanted updates
-    shouldComponentUpdate(nextProps: ICodeDisplayProps) {
+    shouldComponentUpdate(nextProps: ICodeDisplayProps, nextState: ICodeDisplayState) {
         if (nextProps.code.css !== this.state.cssDisplay || this.props.code.css !== nextProps.code.css) {
             this.setState({ cssDisplay: nextProps.code.css })
+            return true;
+        } else if (nextState.displayHTML !== this.state.displayHTML){ //update for html button toggle
             return true;
         } else {
             return false;
@@ -35,13 +39,21 @@ export class CodeDisplay extends React.Component<ICodeDisplayProps, ICodeDisplay
         this.setState({ cssDisplay: event.target.value });
     }
 
+    displayHTMLHandler = () => {
+        this.setState((prevState: ICodeDisplayState) => {
+            return {displayHTML: !prevState.displayHTML};
+          })
+    }
+
     render() {
         const textRowCount = this.state.cssDisplay.split(':').length;
         const rows = textRowCount + 1;
+
         return (
             <div className="codeBlock">
+                <button onClick={this.displayHTMLHandler}>Display HTML</button>
                 <pre>
-                    <code> {this.props.code.html}</code>
+                    {this.state.displayHTML ? <code>{this.props.code.html}</code>: null}
                     <br></br>
                     <br></br>
                     <textarea name={'display'} rows={rows} value={this.state.cssDisplay} onChange={this.updateCSS} />
--- src/examples.ts
+++ src/examples.ts
@@ -2,166 +2,255 @@ import { IAnimationData } from "./container";
 
 export const examples: IAnimationData[] = [
     {
-        description: 'test 1',
-        html: '<a href="#"><span></span>hover me</a>',
-        importantProperties: ['opacity', 'transform'],
+        description: 'Homing Window',
+        html: `<ul>
+        <li>
+            Home
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </li>
+        <li>
+            About
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </li>
+        <li>
+            Services
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </li>
+        <li>
+            Contact
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </li>
+        </ul>`,
+        importantProperties: ['', ''],
         css: `
-    a {
-      width: 220px;
-      height: 80px;
-      color: #ff0;
-      background-color: transparent;
-      font-size: 26px;
-      text-decoration: none;
-      text-transform: uppercase;
-      text-align: center;
-      line-height: 80px;
-      transition: all 0.5s;
-      position: relative;
-    }
-    
-    a:before,
-    a:after {
-      content: "";
-      position: absolute;
-      top: 50%;
-      width: 20px;
-      height: 20px;
-      background-color: #ff0;
-      border-radius: 50%;
-      transform: translateY(-50%);
-      transition: all 0.3s;
-      z-index: -1;
-      opacity: 0;
-    }
-    
-    a:before {
-      left: 0;
-      box-shadow: -100px 0 0 #ff0;
-    }
-    
-    a:after {
-      right: 0;
-      box-shadow: 100px 0 0 #ff0;
-    }
-    
-    a:hover:before {
-      left: 50%;
-      box-shadow: 30px 0 0 #ff0;
-      transform: translateX(-50%) translateY(-50%);
-      opacity: 1;
-    }
-    
-    a:hover:after {
-      right: 50%;
-      box-shadow: -30px 0 0 #ff0;
-      transform: translateX(50%) translateY(-50%);
-      opacity: 1;
-    }
-    
-    span {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background-color: #ff0;
-      border-radius: 8px;
-      transform: scale(0);
-      transition: all 0.3s;
-      z-index: -1;
-    }
-    
-    a:hover span {
-      transform: scale(1);
-      transition-delay: 0.4s;
-    }
-    
-    a:hover {
-      color: #262626;
-      transition-delay: 0.4s;
-    }
+        ul {
+          display: flex;
+          width: 60%;
+          flex-direction: row;
+      }
+      
+      li {
+          flex: 1;
+          display: inline-block;
+          height: 75px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          position: relative;
+          transition: all 0.5s linear;
+          overflow: hidden;
+          border-radius: 8px;
+          margin: 3px;
+          perspective: 900px;
+      }
+      
+      li:hover {
+          color: white;
+      }
+      
+      span {
+          position: absolute;
+          left: 0;
+          top: 0;
+          background-color: blue;
+          height: 100%;
+          width: 25%; 
+          z-index: -1;
+          transform: scale(0) translateY(100%) translateX(100%);
+          transition: all 0.8s ease-in;
+          transform-origin: top;
+      }
+      
+      
+      li:hover span {
+          transform: scale(1) translateY(0) translateX(0);
+      }
+      
+      span:nth-child(2) {
+          left: 12.5%;
+          transition-delay: 0.1s;
+      }
+      
+      span:nth-child(3) {
+          left: 25%;
+          transition-delay: 0.2s;
+      }
+      
+      span:nth-child(4) {
+          left: 37.5%;
+          transition-delay: 0.3s;
+      }
+      
+      span:nth-child(5) {
+          left: 50%;
+          transition-delay: 0.4s;
+      }
+      
+      span:nth-child(6) {
+          left: 62.5%;
+          transition-delay: 0.5s;
+      }
+      
+      span:nth-child(7) {
+          left: 75%;
+          transition-delay: 0.6s;
+      }
+      
+      span:nth-child(8) {
+          left: 87.5%;
+          transition-delay: 0.7s;
+      }
     `
     },
     {
-        description: 'test 2',
-        html: '<a href="#"><span></span>hover me aswell</a>',
-        importantProperties: ['text-decoration', 'border-radius'],
-        css: `a {
-    width: 220px;
-    height: 80px;
-    color: green;
-    background-color: transparent;
-    font-size: 26px;
-    text-transform: uppercase;
-    text-align: center;
-    line-height: 80px;
-    transition: all 0.5s;
-    position: relative;
-  }
-  
-  a:before,
-  a:after {
-    content: "";
-    position: absolute;
-    top: 50%;
-    width: 20px;
-    height: 20px;
-    background-color: #ff0;
-    border-radius: 50%;
-    transform: translateY(-50%);
-    transition: all 0.3s;
-    z-index: -1;
-    opacity: 0;
-  }
-  
-  a:before {
-    left: 0;
-    box-shadow: -100px 0 0 #ff0;
-  }
-  
-  a:after {
-    right: 0;
-    box-shadow: 100px 0 0 #ff0;
-  }
-  
-  a:hover:before {
-    left: 50%;
-    box-shadow: 30px 0 0 #ff0;
-    transform: translateX(-50%) translateY(-50%);
-    opacity: 1;
-  }
-  
-  a:hover:after {
-    right: 50%;
-    box-shadow: -30px 0 0 #ff0;
-    transform: translateX(50%) translateY(-50%);
-    opacity: 1;
-  }
-  
-  span {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #ff0;
-    border-radius: 8px;
-    transform: scale(0);
-    transition: all 0.3s;
-    z-index: -1;
-  }
-  
-  a:hover span {
-    transform: scale(1);
-    transition-delay: 0.4s;
-  }
-  
-  a:hover {
-    color: #262626;
-    transition-delay: 0.4s;
-  }
+        description: 'Fold Message Over Top',
+        html: `<button>Hover Me</button>`,
+        importantProperties: [''],
+        css: `button {
+          border: 2px solid black;
+          height: 100px;
+          width: 200px;
+          position: relative;
+          font-size: 20px;
+          letter-spacing: 2px;
+          font-family: cursive;
+          text-transform: uppercase;
+      }
+      
+      button::before {
+          content: 'Hover Me';
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background-color: greenyellow;
+          position: absolute;
+          top: 0px;
+          left: 0px;
+          height: 100%;
+          width: 100%;
+          transform-origin: top;
+          transform: rotateX(270deg);
+          transition: transform 1s linear;
+      }
+      
+      button::after {
+          content: 'Hover Me';
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background-color: tomato;
+          position: absolute;
+          top: 0px;
+          left: 0px;
+          height: 100%;
+          width: 100%;
+          transform-origin: top;
+          transform: rotateX(270deg);
+          transition: transform 1s linear 1s;
+      }
+      
+      button:hover::before, button:hover::after{
+          transform: rotateX(-0deg);
+      }
   `
+    },
+    {
+      description: 'Horizontal Expansion',
+      html: `<button>button</button>`,
+      importantProperties: [''],
+      css: `
+      button {
+        color: red;
+        width: 200px;
+        height: 100px;
+        border: 2px solid red;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        transition: all 0.5s linear;
+        font-size: 16px;
+        font-family: sans-serif;
+        font-weight: 300;
     }
-
+    
+    button:hover {
+        width: 250px;
+        border: 3px solid red;
+        letter-spacing: 10px;
+        font-weight: 600;
+    }
+    `},
+    {
+      description: 'Shiny Button',
+      html: `<a href="">hover me</a>`,
+      importantProperties: [''],
+      css: `
+      a {
+        text-decoration: none;
+        border-radius: 6px;
+        border: 2px solid white;
+        color: white;
+        background-color: #999;
+        padding: 40px;
+        width: 200px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-sizing: border-box;
+        transition: all 1s ease-out;
+        position: relative;
+        overflow: hidden;
+    
+    }
+    
+    a::before {
+        content: '';
+        width: 140%;
+        height: 40%;
+        background-color: white;
+        position: absolute;
+        bottom: -50%;
+        left: -70%;
+        z-index -1;
+        transform: rotateZ(50deg);
+        transform-origin: top;
+        transition: all 1s;
+    }
+    
+    a:hover{
+        background-color: red;
+    }
+    
+    a:hover::before{
+        transform: rotateZ(50deg) translateY(-300px);
+    }
+    
+      `}
 ]
